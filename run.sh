b030ac26eaf92d5a492040df0bcb3ea242a0bb69
zhu-chen <1040555811@qq.com>
2025-03-11 13:49:57 +0800

    perf:improve log in run.sh

--- run.sh
+++ run.sh
@@ -10,37 +10,60 @@ done
 
 
 #Cora
+echo -e "==================\n\n Cora\n" 
 #none
+echo -e "none\n" 
 python -u main.py --dataset=Cora --homo_loss_weight=0.1 --target_loss_weight=1 --vs_number=10 --test_model=GCN --defense_mode=none --epochs=200 --trojan_epochs=200 --device_id=$DEVICE_ID  --alpha_int=30 --hidden=80 --shadow_lr=0.0002 --trojan_lr=0.0002
 #prune
+echo -e "prune\n" 
 python -u main.py --prune_thr=0.1 --dataset=Cora --homo_loss_weight=1 --target_loss_weight=1 --vs_number=10 --test_model=GCN --defense_mode=prune --epochs=200 --trojan_epochs=200 --device_id=$DEVICE_ID  --alpha_int=30 --hidden=80 --shadow_lr=0.0003 --trojan_lr=0.0003
 #od
+echo -e "od\n" 
 python -u main.py --dataset=Cora --homo_loss_weight=5 --target_loss_weight=4 --vs_number=10 --test_model=GCN --defense_mode=reconstruct --epochs=200 --trojan_epochs=200 --device_id=$DEVICE_ID  --alpha_int=30 --hidden=80 --shadow_lr=0.0003 --trojan_lr=0.0003
 #rigbd
+echo -e "rigbd\n" 
 python -u main_rigbd.py --dataset=Cora --homo_loss_weight=0.1 --target_loss_weight=1 --vs_number=10 --test_model=GCN --defense_mode=none --epochs=200 --trojan_epochs=200 --device_id=$DEVICE_ID  --alpha_int=30 --hidden=80 --shadow_lr=0.0002 --trojan_lr=0.0002
+
 #Pubmed
+echo -e"==================\n\n Pubmed\n" 
 #none
+echo -e "none\n" 
 python -u main.py --dataset=Pubmed --homo_loss_weight=0.1 --vs_number=40 --test_model=GCN --defense_mode=none --epochs=200 --trojan_epochs=400 --device_id=$DEVICE_ID  --alpha_int=10 --hidden=64 --target_class=2 --shadow_lr=0.01 --trojan_lr=0.01
 #prune
+echo -e "prune\n" 
 python -u main.py --prune_thr=0.2 --dataset=Pubmed --homo_loss_weight=2 --vs_number=40 --test_model=GCN --defense_mode=prune --epochs=200 --trojan_epochs=400 --device_id=$DEVICE_ID  --alpha_int=10 --hidden=64 --target_class=2 --shadow_lr=0.01 --trojan_lr=0.01
 #od
+echo -e "od\n" 
 python -u main.py --dataset=Pubmed --homo_loss_weight=1 --target_loss_weight=1 --vs_number=157 --test_model=GCN --defense_mode=reconstruct --epochs=200 --trojan_epochs=500 --device_id=$DEVICE_ID  --alpha_int=10 --hidden=64 --target_class=2 --shadow_lr=0.01 --trojan_lr=0.01
 #rigbd
+echo -e "rigbd\n" 
 python -u main_rigbd.py --dataset=Pubmed --homo_loss_weight=1 --vs_number=40 --test_model=GCN --epochs=200 --trojan_epochs=400 --train_lr=0.01 --device_id=$DEVICE_ID  --alpha_int=10 --hidden=64 --target_class=2 --shadow_lr=0.01 --trojan_lr=0.01
+
 #OGB-arxiv
+echo -e "==================\n\n OGB-arxiv\n" 
 #none
+echo -e "none\n" 
 python -u main.py --dataset=ogbn-arxiv --homo_loss_weight=0 --vs_number=565 --test_model=GCN --defense_mode=none --epochs=800 --trojan_epochs=800 --device_id=$DEVICE_ID  --alpha_int=5 --hidden=80 --outter_size=256 --shadow_lr=0.001 --trojan_lr=0.001
 #prune
+echo -e "prune\n" 
 python -u main.py --dataset=ogbn-arxiv --homo_loss_weight=4 --vs_number=565 --test_model=GCN --defense_mode=prune --epochs=800 --trojan_epochs=800 --device_id=$DEVICE_ID  --alpha_int=5 --hidden=80 --outter_size=256 --shadow_lr=0.001 --trojan_lr=0.001
 #od
+echo -e "od\n"
 python -u main.py --dataset=ogbn-arxiv --homo_loss_weight=10 --vs_number=1693 --test_model=GCN --defense_mode=reconstruct --epochs=800 --trojan_epochs=600 --device_id=$DEVICE_ID  --alpha_int=5 --hidden=64 --outter_size=512 --shadow_lr=0.001 --trojan_lr=0.001
 #rigbd
+echo -e "rigbd\n"
 python -u main_rigbd.py --dataset=ogbn-arxiv --homo_loss_weight=0 --vs_number=565 --test_model=GCN --defense_mode=none --epochs=800 --trojan_epochs=800 --device_id=$DEVICE_ID  --alpha_int=5 --hidden=80 --outter_size=256 --shadow_lr=0.001 --trojan_lr=0.001
+
+
 #GAT--Need at least 25G GPU memory
+echo -e "++++++\n GAT\n"
 python -u main.py --dataset=ogbn-arxiv --homo_loss_weight=0 --vs_number=565 --test_model=GAT --epochs=800 --train_lr=0.01 --trojan_epochs=800 --device_id=0 --alpha_int=5 --hidden=80 --outter_size=256 --shadow_lr=0.001 --trojan_lr=0.001
 #GrapgSAGE
+echo -e "++++++\n GrapgSAGE\n"
 python -u main.py --dataset=ogbn-arxiv --homo_loss_weight=5 --vs_number=565 --test_model=GraphSage --epochs=800 --train_lr=0.01 --trojan_epochs=800 --device_id=$DEVICE_ID  --alpha_int=5 --hidden=80 --outter_size=256 --shadow_lr=0.001 --trojan_lr=0.001
 #GNNGuard
+echo -e "++++++\n GNNGuard\n"
 python -u main.py --dataset=ogbn-arxiv --homo_loss_weight=5 --vs_number=565 --test_model=GNNGuard --epochs=800 --train_lr=0.01 --trojan_epochs=800 --device_id=$DEVICE_ID  --alpha_int=5 --hidden=80 --outter_size=256 --shadow_lr=0.001 --trojan_lr=0.001
 #RobustGCN--Take several hours on 4090.
+echo -e "++++++\n RobustGCN\n"
 python -u main.py --dataset=ogbn-arxiv --homo_loss_weight=5 --vs_number=565 --test_model=RobustGCN --epochs=800 --train_lr=0.01 --trojan_epochs=800 --device_id=$DEVICE_ID  --alpha_int=5 --hidden=80 --outter_size=256 --shadow_lr=0.001 --trojan_lr=0.001
